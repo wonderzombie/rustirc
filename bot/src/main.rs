f7357bb278db325409c9e60ab0b38d5950855864
wz <wonderzombie@gmail.com>
2025-11-10 17:45:22 -0800

    nits: format file

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,8 +1,8 @@
 mod example_handler;
 
+use clap::Parser;
 use irc_core;
 use irc_core::bot;
-use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -19,7 +19,10 @@ struct Args {
 async fn main() -> anyhow::Result<()> {
     println!("=== Hello, world!");
     let args = Args::parse();
-    println!("=== Using nick: {}, user: {}, server: {}", args.nick, args.user, args.server);
+    println!(
+        "=== Using nick: {}, user: {}, server: {}",
+        args.nick, args.user, args.server
+    );
 
     let client = irc_core::connect(args.server, args.nick, args.user).await?;
     let bot = bot::BotBuilder::new()
