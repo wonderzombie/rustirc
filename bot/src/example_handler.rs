ec23dc7fcedb764abd9764064af68b471ac0b0ab
wz <wonderzombie@gmail.com>
2025-11-10 17:10:12 -0800

    example_handler handles ping/pong, joining channel, echoes chats

--- bot/src/example_handler.rs
+++ bot/src/example_handler.rs
@@ -2,26 +2,36 @@ use std::ops::ControlFlow;
 
 use crate::irc_core::{handler, irc_msg};
 
-
 pub struct ExampleHandler;
 
 #[async_trait::async_trait]
 impl handler::Handler for ExampleHandler {
-    async fn handle(
-        &self,
-        ctx: &handler::Context,
-        msg: &irc_msg::Msg,
-    ) -> ControlFlow<()> {
-        use crate::irc_core::irc_msg::Command;
-
-        if let Command::Privmsg { channel, message, .. } = &msg.command {
-            if message == "!hello" {
-                let reply = format!("Hello! You said: {}", message);
-                if let Err(e) = ctx.client.privmsg(channel, &reply).await {
-                    eprintln!("Failed to send PRIVMSG: {:?}", e);
-                }
+    async fn handle(&self, ctx: &handler::Context, msg: &irc_msg::Msg) -> ControlFlow<()> {
+        match msg.command {
+            irc_msg::Command::Ping { ref token } => {
+                let _ = ctx.client.pong(token.as_deref()).await;
                 return ControlFlow::Break(());
             }
+
+            irc_msg::Command::Numeric { code, .. } => {
+                // End of MOTD, join a channel
+                match code {
+                    376 | 422 => {
+                        println!("=== Joined server");
+                        let _ = ctx.client.join("#el_rb_test376").await;
+                        return ControlFlow::Break(());
+                    }
+                    _ => return ControlFlow::Continue(()),
+                }
+            }
+            irc_msg::Command::Privmsg {
+                ref channel,
+                ref message,
+                ..
+            } => {
+                println!("=== PRIVMSG {} :{}", channel, message);
+            }
+            _ => {}
         }
 
         ControlFlow::Continue(())
