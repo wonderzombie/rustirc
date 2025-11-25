145d8d487e3c1817a737fbe3e183b50d775483cd
wz <wonderzombie@gmail.com>
2025-11-24 17:28:31 -0800

    apply standard formatting

--- bot/src/score.rs
+++ bot/src/score.rs
@@ -14,11 +14,11 @@ impl PrivmsgHandler for ScoreHandler {
         message: &str,
     ) -> ControlFlow<()> {
         let in_channel = {
-          let state = ctx.state.lock().await;
-          state.channels.iter().any(|s| channel == s)
+            let state = ctx.state.lock().await;
+            state.channels.iter().any(|s| channel == s)
         };
         if !in_channel {
-          return ControlFlow::Continue(());
+            return ControlFlow::Continue(());
         }
 
         let delta: Option<(&str, i32)> = parse_score_delta(message);
@@ -36,18 +36,22 @@ impl PrivmsgHandler for ScoreHandler {
 }
 
 fn parse_score_delta(message: &str) -> Option<(&str, i32)> {
-  let mut delta: Option<(&str, i32)> = None;
+    let mut delta: Option<(&str, i32)> = None;
 
-  for token in message.split_ascii_whitespace() {
-      if let Some((nick, _)) = token.split_once("++") && nick != "" {
-          delta = Some((nick, 1));
-          break;
-      } else if let Some((nick, _)) = token.split_once("--") && nick != "" {
-          delta = Some((nick, -1));
-          break;
-      }
-  }
-  delta
+    for token in message.split_ascii_whitespace() {
+        if let Some((nick, _)) = token.split_once("++")
+            && nick != ""
+        {
+            delta = Some((nick, 1));
+            break;
+        } else if let Some((nick, _)) = token.split_once("--")
+            && nick != ""
+        {
+            delta = Some((nick, -1));
+            break;
+        }
+    }
+    delta
 }
 
 impl ScoreHandler {
@@ -56,51 +60,48 @@ impl ScoreHandler {
             .entry(nick.to_string())
             .and_modify(|it| *it += d)
             .or_insert_with(|| d);
-
-
     }
 }
 
 #[cfg(test)]
 mod tests {
 
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn test_parse_score_delta() {
-    let delta = parse_score_delta("message++");
-    assert_eq!(delta, Some(("message", 1)));
-    let delta = parse_score_delta("message--");
-    assert_eq!(delta, Some(("message", -1)));
-  }
+    #[test]
+    fn test_parse_score_delta() {
+        let delta = parse_score_delta("message++");
+        assert_eq!(delta, Some(("message", 1)));
+        let delta = parse_score_delta("message--");
+        assert_eq!(delta, Some(("message", -1)));
+    }
 
-  #[test]
-  fn test_parse_score_delta_invalid() {
-    let delta = parse_score_delta("message+-");
-    assert_eq!(delta, None);
-    let delta = parse_score_delta("++");
-    assert_eq!(delta, None);
-    let delta = parse_score_delta("--message");
-    assert_eq!(delta, None);
-  }
+    #[test]
+    fn test_parse_score_delta_invalid() {
+        let delta = parse_score_delta("message+-");
+        assert_eq!(delta, None);
+        let delta = parse_score_delta("++");
+        assert_eq!(delta, None);
+        let delta = parse_score_delta("--message");
+        assert_eq!(delta, None);
+    }
 
-  #[test]
-  fn test_add_to_score() {
-    let mut scores: HashMap<String, i32> = HashMap::new();
-    scores.insert("botty".into(), 1);
-    scores.insert("thumbkin".into(), -1);
+    #[test]
+    fn test_add_to_score() {
+        let mut scores: HashMap<String, i32> = HashMap::new();
+        scores.insert("botty".into(), 1);
+        scores.insert("thumbkin".into(), -1);
 
-    ScoreHandler::add_to_score(&mut scores, "botty", 1);
-    let new_score = scores.get("botty").copied();
-    assert_eq!(Some(2), new_score);
+        ScoreHandler::add_to_score(&mut scores, "botty", 1);
+        let new_score = scores.get("botty").copied();
+        assert_eq!(Some(2), new_score);
 
-    ScoreHandler::add_to_score(&mut scores, "thumbkin", -1);
-    let new_score = scores.get("thumbkin").copied();
-    assert_eq!(Some(-2), new_score);
+        ScoreHandler::add_to_score(&mut scores, "thumbkin", -1);
+        let new_score = scores.get("thumbkin").copied();
+        assert_eq!(Some(-2), new_score);
 
-    ScoreHandler::add_to_score(&mut scores, "beelzebub", 1);
-    let new_score = scores.get("beelzebub").copied();
-    assert_eq!(Some(1), new_score);
-  }
-
+        ScoreHandler::add_to_score(&mut scores, "beelzebub", 1);
+        let new_score = scores.get("beelzebub").copied();
+        assert_eq!(Some(1), new_score);
+    }
 }
